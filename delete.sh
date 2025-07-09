a58de2a1ee66eefdf6d41257545296dc87e3d68c
Daniel Ramos <danielramosacosta1@gmail.com>
2025-07-09 07:17:19 +0200

    all working

---
+++ delete.sh
@@ -0,0 +1,12 @@
+kubectl delete all --all -n default
+kubectl delete pvc --all -n default
+kubectl delete configmap --all -n default
+kubectl delete secret --all -n default
+kubectl delete ingress --all -n default
+kubectl delete serviceaccount --all -n default
+kubectl delete cronjobs --all -n default
+kubectl delete jobs --all -n default
+kubectl delete role --all -n default
+kubectl delete rolebinding --all -n default
+kubectl delete networkpolicy --all -n default
+kubectl delete horizontalpodautoscaler --all -n default
