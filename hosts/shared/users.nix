f03fcc82f9c34cc8761434f98a6dc81a985c38ce
Daniel Ramos <danielramosacosta1@gmail.com>
2025-07-14 19:26:09 +0000

    all working now

--- hosts/shared/users.nix
+++ hosts/shared/users.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
-  # age.secrets.dani-hashed-password.file = "${self}/secrets/dani-hashed-password.age";
+  age.secrets.dani-hashed-password.file = ../../secrets/dani-hashed-password.age;
 
   users = {
     mutableUsers = false;
@@ -9,8 +9,7 @@
       dani = {
         isNormalUser = true;
         extraGroups = [ "wheel" ];
-        # hashedPasswordFile = config.age.secrets.dani-hashed-password.path;
-        password = "changeme";
+        hashedPasswordFile = config.age.secrets.dani-hashed-password.path;
         openssh.authorizedKeys.keys = [
           (builtins.readFile ../../id_dani.pub)
         ];
