f73ac4183f2e90ef1d87d1d84a0eab6a06c4a8af
Daniel Ramos <danielramosacosta1@gmail.com>
2025-10-03 20:47:14 +0000

    update fans config

--- hosts/shared/services/fans.nix
+++ hosts/shared/services/fans.nix
@@ -8,19 +8,19 @@
     fans = [
       {
         name = "fan1";
-        percentage = 30;
+        percentage = 60;
       }
       {
         name = "fan2";
-        percentage = 30;
+        percentage = 60;
       }
       {
         name = "fan3";
-        percentage = 30;
+        percentage = 50;
       }
       {
         name = "fan4";
-        percentage = 30;
+        percentage = 50;
       }
     ];
   };
