a7687bd0727a939cd3ab7924c3368520d14b8a13
Daniel Ramos <danielramosacosta1@gmail.com>
2026-01-05 14:00:52 +0000

    add hardware acceletartion

--- hosts/nas/configuration.nix
+++ hosts/nas/configuration.nix
@@ -5,11 +5,20 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
-    
+
     initrd = {
       supportedFilesystems = [ "btrfs" ];
       systemd.emergencyAccess = true;
     };
+  };
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver
+      libva
+      libva-utils
+    ];
   };
 }
 
