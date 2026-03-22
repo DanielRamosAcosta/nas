c7675d632c0a27f540fb2591cc32d7dacb4bfdef
Daniel Ramos <danielramosacosta1@gmail.com>
2026-03-22 01:59:53 +0100

    Move dhcpcd.denyInterfaces from hardware-configuration.nix to k3s.nix

--- hosts/nas/services/k3s.nix
+++ hosts/nas/services/k3s.nix
@@ -2,6 +2,7 @@
 {
   networking.firewall.allowedTCPPorts = [ 6443 ];
   networking.firewall.trustedInterfaces = ["cni0" "flannel.1"];
+  networking.dhcpcd.denyInterfaces = [ "veth*" "cni*" "flannel*" ];
 
   services.k3s = {
     enable = true;
